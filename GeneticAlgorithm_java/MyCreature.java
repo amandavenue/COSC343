25f9a01cbe111d0d8b20e79e5ca22f12866d8362
Amanda Veldman <aveldman@oucs1509.otago.ac.nz>
2018-05-12 15:21:56 +1200

    worked on creature behaviours

--- GeneticAlgorithm_java/MyCreature.java
+++ GeneticAlgorithm_java/MyCreature.java
@@ -12,47 +12,126 @@ import java.util.Random;
 */
 public class MyCreature extends Creature {
 
-  // Random number generator
-  Random rand = new Random();
+    // Random number generator
+    Random rand = new Random();
+    public int[] chromosome;
 
-  /* Empty constructor - might be a good idea here to put the code that 
-   initialises the chromosome to some random state   
+    /* Constructor to make a creatures chromosome.
+    Chromosome is 11 ints between 1 and 100  
   
-   Input: numPercept - number of percepts that creature will be receiving
+    Input: numPercept - number of percepts that creature will be receiving
           numAction - number of action output vector that creature will need
                       to produce on every turn
-  */
-  public MyCreature(int numPercepts, int numActions) {
+    */
+    public MyCreature(int numPercepts, int numActions) {
 
-  }
+        chromosome = new int [11];
+
+        for (int i = 0; i < chromosome.length; i++){
+            chromosome[i] = rand.nextInt(101);
+        }
+    }
+    /* This function must be overridden by the MyCreature class, because it implements
+        the AgentFunction which controls creature behavoiur.  This behaviour
+        should be governed by the model (that you need to come up with) that is
+        parameterise by the chromosome.  
   
-  /* This function must be overridden by the MyCreature class, because it implements
-     the AgentFunction which controls creature behavoiur.  This behaviour
-     should be governed by the model (that you need to come up with) that is
-     parameterise by the chromosome.  
-  
-     Input: percepts - an array of percepts
-            numPercepts - the size of the array of percepts depend on the percept
-                          chosen
-            numExpectedAction - this number tells you what the expected size
-                                of the returned array of percepts should bes
-     Returns: an array of actions 
-  */
-  @Override
-  public float[] AgentFunction(int[] percepts, int numPercepts, int numExpectedActions) {
+        Input: percepts - an array of percepts
+                numPercepts - the size of the array of percepts depend on the percept
+                            chosen
+                numExpectedAction - this number tells you what the expected size
+                                    of the returned array of percepts should bes
+         Returns: an array of actions 
+     */
+    @Override
+    public float[] AgentFunction(int[] percepts, int numPercepts, int numExpectedActions) {
       
-      // This is where your chromosome gives rise to the model that maps
-      // percepts to actions.  This function governs your creature's behaviour.
-      // You need to figure out what model you want to use, and how you're going
-      // to encode its parameters in a chromosome.
+        // This is where your chromosome gives rise to the model that maps
+        // percepts to actions.  This function governs your creature's behaviour.
+        // You need to figure out what model you want to use, and how you're going
+        // to encode its parameters in a chromosome.
       
-      // At the moment, the actions are chosen completely at random, ignoring
-      // the percepts.  You need to replace this code.
-      float actions[] = new float[numExpectedActions];
-      for(int i=0;i<numExpectedActions;i++) {
-         actions[i]=rand.nextFloat();
-      } 
-      return actions;
-  }
+        // At the moment, the actions are chosen completely at random, ignoring
+        // the percepts.  You need to replace this code.
+        float actions[] = new float[numExpectedActions];
+        int numMonsters = 0;
+        int avoidDirection;
+
+        int topLeft = (chromosome[3] + chromosome[0] + chromosome[1])/3;
+        int topRight = (chromosome[1] + chromosome[2] + chromosome[5])/3;
+        int bottomLeft = (chromosome[3] + chromosome[6] + chromosome[7])/3;
+        int bottomRight = (chromosome[5] + chromosome[8] + chromosome[9])/3;
+
+        /*
+        for(int i=0;i<numExpectedActions;i++) {
+            actions[i]=rand.nextFloat();
+        } 
+        */
+
+        // if creature is on edible food
+        if (percepts[4] == 2){
+            //eat food at the probability of chromosome[9]
+            if (rand.nextInt(101) < chromosome[9]){
+                actions[9] = Float.MAX_VALUE;
+            }
+
+        // if creature is on green food
+        } else if (percepts[4] == 1){
+            for (int i = 0; i < numPercepts; i++){
+                // if there is any monsters around set the monster boolean to 
+                if (i != 4 && percepts[i] == 1){
+                    numMonsters++;
+                }
+            }
+            // if there are no monsters around eatthe green food at the probability of chromosome[9]
+            if (numMonsters > 0 && rand.nextInt(101) < chromosome[9]){
+                actions[9] = Float.MAX_VALUE;
+            }
+
+        // if creature isnt on food but there is a monster nearby
+        } else if (numMonsters == 1){
+    
+            // avoid based on the chromosome corresponding to the location percept
+            for (int i = 0; i < numPercepts; i++){
+                if (i != 4 && percepts[i] == 1 && rand.nextInt(101) < chromosome[i]){
+                    if (i < 3){
+                        actions[(i+5)%9] = Float.MAX_VALUE;
+                    } else {
+                        actions[(i+4)%9] = Float.MAX_VALUE;
+                    }
+                }
+            }
+
+        // multiple monsers
+        } else if (numMonsters > 1){
+
+            for(int i=0;i<numExpectedActions;i++) {
+            actions[i]=rand.nextFloat();
+            }
+
+        // no monsters around
+        } else {
+            // if there is a berry near by move towards it if directional chromosome probability is high enough
+            for(int i = 0; i < numPercepts; i++){
+                if (i != 4 && percepts[i] == 3 && rand.nextInt(101) < chromosome[i]){
+                    actions[i] = Float.MAX_VALUE;
+                    return actions;
+                }
+            }
+            // explore
+            if (rand.nextInt(101) < chromosome[numPercepts]){
+                actions[numPercepts] = Float.MAX_VALUE;
+                return actions;
+            }
+
+            // random move fail case
+            for(int i=0;i<numExpectedActions;i++) {
+            actions[i]=rand.nextFloat();
+            }
+
+        }
+
+        return actions;
+    }
   
 }
--- GeneticAlgorithm_java/MyWorld.java
+++ GeneticAlgorithm_java/MyWorld.java
@@ -14,161 +14,161 @@ import java.util.*;
 */
 public class MyWorld extends World {
  
-  /* Here you can specify the number of turns in each simulation
-   * and the number of generations that the genetic algorithm will 
-   * execute.
-  */
-  private final int _numTurns = 100;
-  private final int _numGenerations = 500;
+	/* Here you can specify the number of turns in each simulation
+	* and the number of generations that the genetic algorithm will 
+	* execute.
+	*/
+	private final int _numTurns = 100;
+	private final int _numGenerations = 500;
   
 
   
-  /* Constructor.  
+	/* Constructor.  
    
-     Input: worldType - specifies which simulation will be running
-            griSize - the size of the world
-            windowWidth - the width (in pixels) of the visualisation window
-            windowHeight - the height (in pixels) of the visualisation window
-            repeatableMode - if set to true, every simulation in each
-                             generation will start from the same state
-  */
-  public MyWorld(int worldType, int gridSize, int windowWidth, int windowHeight, boolean repeatableMode) {   
-      // Initialise the parent class - don't remove this
-      super(worldType, gridSize, windowWidth,  windowHeight, repeatableMode);
+		Input: worldType - specifies which simulation will be running
+			griSize - the size of the world
+			windowWidth - the width (in pixels) of the visualisation window
+			windowHeight - the height (in pixels) of the visualisation window
+			repeatableMode - if set to true, every simulation in each
+							 generation will start from the same state
+	*/
+	public MyWorld(int worldType, int gridSize, int windowWidth, int windowHeight, boolean repeatableMode) {   
+	   	// Initialise the parent class - don't remove this
+	   	super(worldType, gridSize, windowWidth,  windowHeight, repeatableMode);
 
-      // Set the number of turns and generations
-      this.setNumTurns(_numTurns);
-      this.setNumGenerations(_numGenerations);
-      
-      
-  }
+	   	// Set the number of turns and generations
+	   	this.setNumTurns(_numTurns);
+	   	this.setNumGenerations(_numGenerations);
+	  
+	  
+	}
  
-  /* The main function for the MyWorld application
+	/* The main function for the MyWorld application
 
-  */
-  public static void main(String[] args) {
-     // Here you can specify the grid size, window size and whether to run
-     // in repeatable mode or not
-     int gridSize = 24;
-     int windowWidth =  1600;
-     int windowHeight = 900;
-     boolean repeatableMode = false;
-     
-      /* Here you can specify world type - there are two to
-         choose from: 1 and 2.  Refer to the Assignment2 instructions for
-         explanation of the world type formats.
-      */
-     int worldType = 1;     
-     
-     // Instantiate MyWorld object.  The rest of the application is driven
-     // from the window that will be displayed.
-     MyWorld sim = new MyWorld(worldType, gridSize, windowWidth, windowHeight, repeatableMode);
-  }
+	*/
+	public static void main(String[] args) {
+	  	// Here you can specify the grid size, window size and whether to run
+	   	// in repeatable mode or not
+	   	int gridSize = 24;
+	   	int windowWidth =  1600;
+	   	int windowHeight = 900;
+	   	boolean repeatableMode = false;
+	 
+	   	/* Here you can specify world type - there are two to
+		 choose from: 1 and 2.  Refer to the Assignment2 instructions for
+		 explanation of the world type formats.
+	   	*/
+	   	int worldType = 1;     
+	 
+	   	// Instantiate MyWorld object.  The rest of the application is driven
+	   	// from the window that will be displayed.
+	   	MyWorld sim = new MyWorld(worldType, gridSize, windowWidth, windowHeight, repeatableMode);
+	}
   
 
-  /* The MyWorld class must override this function, which is
-     used to fetch a population of creatures at the beginning of the
-     first simulation.  This is the place where you need to  generate
-     a set of creatures with random behaviours.
+	/* The MyWorld class must override this function, which is
+	 used to fetch a population of creatures at the beginning of the
+	 first simulation.  This is the place where you need to  generate
+	 a set of creatures with random behaviours.
   
-     Input: numCreatures - this variable will tell you how many creatures
-                           the world is expecting
-                            
-     Returns: An array of MyCreature objects - the World will expect numCreatures
-              elements in that array     
-  */  
-  @Override
-  public MyCreature[] firstGeneration(int numCreatures) {
+	 Input: numCreatures - this variable will tell you how many creatures
+						   the world is expecting
+							
+	 Returns: An array of MyCreature objects - the World will expect numCreatures
+			  elements in that array     
+	*/  
+	@Override
+	public MyCreature[] firstGeneration(int numCreatures) {
 
-    int numPercepts = this.expectedNumberofPercepts();
-    int numActions = this.expectedNumberofActions();
-      
-    // This is just an example code.  You may replace this code with
-    // your own that initialises an array of size numCreatures and creates
-    // a population of your creatures
-    MyCreature[] population = new MyCreature[numCreatures];
-    for(int i=0;i<numCreatures;i++) {
-        population[i] = new MyCreature(numPercepts, numActions);     
-    }
-    return population;
-  }
+	   	int numPercepts = this.expectedNumberofPercepts();
+	   	int numActions = this.expectedNumberofActions();
+	  
+	   	// This is just an example code.  You may replace this code with
+	   	// your own that initialises an array of size numCreatures and creates
+	   	// a population of your creatures
+	   	MyCreature[] population = new MyCreature[numCreatures];
+	   	for(int i=0;i<numCreatures;i++) {
+			population[i] = new MyCreature(numPercepts, numActions);     
+		}
+		return population;
+  	}
   
-  /* The MyWorld class must override this function, which is
-     used to fetch the next generation of creatures.  This World will
-     proivde you with the old_generation of creatures, from which you can
-     extract information relating to how they did in the previous simulation...
-     and use them as parents for the new generation.
+  	/* The MyWorld class must override this function, which is
+	 used to fetch the next generation of creatures.  This World will
+	 proivde you with the old_generation of creatures, from which you can
+	 extract information relating to how they did in the previous simulation...
+	 and use them as parents for the new generation.
   
-     Input: old_population_btc - the generation of old creatures before type casting. 
-                              The World doesn't know about MyCreature type, only
-                              its parent type Creature, so you will have to
-                              typecast to MyCreatures.  These creatures 
-                              have been participating in a simulation and their state
-                              can be queried to evaluate their fitness
-            numCreatures - the number of elements in the old_population_btc
-                           array
-                        
-                            
-  Returns: An array of MyCreature objects - the World will expect numCreatures
-           elements in that array.  This is the new population that will be
-           used for the next simulation.  
-  */  
-  @Override
-  public MyCreature[] nextGeneration(Creature[] old_population_btc, int numCreatures) {
-    // Typcast old_population of Creatures to array of MyCreatures
-     MyCreature[] old_population = (MyCreature[]) old_population_btc;
-     // Create a new array for the new population
-     MyCreature[] new_population = new MyCreature[numCreatures];
-     
-     // Here is how you can get information about the old creatures and how
-     // well they did in the simulation
-     float avgLifeTime=0f;
-     int nSurvivors = 0;
-     for(MyCreature creature : old_population) {
-        // The energy of the creature.  This is zero if a creature starved to
-        // death, non-negative otherwise.  If this number is non-zero, but the 
-        // creature is dead, then this number gives the energy of the creature
-        // at the time of death.
-        int energy = creature.getEnergy();
+	 Input: old_population_btc - the generation of old creatures before type casting. 
+							  The World doesn't know about MyCreature type, only
+							  its parent type Creature, so you will have to
+							  typecast to MyCreatures.  These creatures 
+							  have been participating in a simulation and their state
+							  can be queried to evaluate their fitness
+			numCreatures - the number of elements in the old_population_btc
+						   array
+						
+							
+  	Returns: An array of MyCreature objects - the World will expect numCreatures
+		   elements in that array.  This is the new population that will be
+		   used for the next simulation.  
+  	*/  
+  	@Override
+  	public MyCreature[] nextGeneration(Creature[] old_population_btc, int numCreatures) {
+		// Typcast old_population of Creatures to array of MyCreatures
+	 	MyCreature[] old_population = (MyCreature[]) old_population_btc;
+	 	// Create a new array for the new population
+	 	MyCreature[] new_population = new MyCreature[numCreatures];
+	 
+	 	// Here is how you can get information about the old creatures and how
+	 	// well they did in the simulation
+	 	float avgLifeTime=0f;
+	 	int nSurvivors = 0;
+	 	for(MyCreature creature : old_population) {
+			// The energy of the creature.  This is zero if a creature starved to
+			// death, non-negative otherwise.  If this number is non-zero, but the 
+			// creature is dead, then this number gives the energy of the creature
+			// at the time of death.
+			int energy = creature.getEnergy();
 
-        // This querry can tell you if the creature died during the simulation
-        // or not.  
-        boolean dead = creature.isDead();
-        
-        if(dead) {
-           // If the creature died during simulation, you can determine
-           // its time of death (in units of turns)
-           int timeOfDeath = creature.timeOfDeath();
-           avgLifeTime += (float) timeOfDeath;
-        } else {
-           nSurvivors += 1;
-           avgLifeTime += (float) _numTurns;
-        }
-     }
+			// This querry can tell you if the creature died during the simulation
+			// or not.  
+			boolean dead = creature.isDead();
+		
+			if(dead) {
+		   		// If the creature died during simulation, you can determine
+		   		// its time of death (in units of turns)
+		   		int timeOfDeath = creature.timeOfDeath();
+		   		avgLifeTime += (float) timeOfDeath;
+			} else {
+		   		nSurvivors += 1;
+		   		avgLifeTime += (float) _numTurns;
+			}
+	 	}
 
-     // Right now the information is used to print some stats...but you should
-     // use this information to access creatures' fitness.  It's up to you how
-     // you define your fitness function.  You should add a print out or
-     // some visual display of the average fitness over generations.
-     avgLifeTime /= (float) numCreatures;
-     System.out.println("Simulation stats:");
-     System.out.println("  Survivors    : " + nSurvivors + " out of " + numCreatures);
-     System.out.println("  Avg life time: " + avgLifeTime + " turns");
+	 	// Right now the information is used to print some stats...but you should
+	 	// use this information to access creatures' fitness.  It's up to you how
+	 	// you define your fitness function.  You should add a print out or
+	 	// some visual display of the average fitness over generations.
+	 	avgLifeTime /= (float) numCreatures;
+	 	System.out.println("Simulation stats:");
+	 	System.out.println("  Survivors    : " + nSurvivors + " out of " + numCreatures);
+	 	System.out.println("  Avg life time: " + avgLifeTime + " turns");
 
-     
-     // Having some way of measuring the fitness, you should implement a proper
-     // parent selection method here and create a set of new creatures.  You need
-     // to create numCreatures of the new creatures.  If you'd like to implement
-     // elitism, you can use old creatures in the next generation.  This
-     // example code uses all the creatures from the old generation in the
-     // new generation.
-     for(int i=0;i<numCreatures; i++) {
-        new_population[i] = old_population[i];
-     }
-     
-     
-     // Return new population of cratures.
-     return new_population;
-  }
+	 
+	 	// Having some way of measuring the fitness, you should implement a proper
+	 	// parent selection method here and create a set of new creatures.  You need
+	 	// to create numCreatures of the new creatures.  If you'd like to implement
+	 	// elitism, you can use old creatures in the next generation.  This
+	 	// example code uses all the creatures from the old generation in the
+	 	// new generation.
+	 	for(int i=0;i<numCreatures; i++) {
+			new_population[i] = old_population[i];
+	 	}
+	 
+	 
+	 	// Return new population of cratures.
+	 	return new_population;
+  	}
   
 }
